014922ec76a4ca21baf456963dddde7bb7890d57
Matt <Mattwildeusa@icloud.com>
2024-10-24 16:36:50 +0100

    moi

--- Untitled-1.py
+++ Untitled-1.py
@@ -6,6 +6,21 @@ length = 30
 N = 7
 T = 4
 epsilon = T/N
+m = 1
+hbar = 1
+
+def propagator(xa,ta,xb,tb):
+
+    A = 1
+    
+
+    return A*integral()
+
+
+def integral(a,b,function,d):
+    return 1
+
+
 
 def potiential(x):
     return 0.5*x**2
