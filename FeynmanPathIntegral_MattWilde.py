e6e40317d34f4ad1032654975f5b07a83a708680
Matt <Mattwildeusa@icloud.com>
2024-11-25 22:06:26 +0000

    Metropolis working wow

--- FeynmanPathIntegral_MattWilde.py
+++ FeynmanPathIntegral_MattWilde.py
@@ -16,7 +16,6 @@ dx = 0.05
 dt = T/N
 TotalXs = int(((xrange)/dx)+1)
 
-
 xf1 = 3
 xi1 = -3
 xrange1 = xf1-xi1
@@ -39,39 +38,39 @@ def action(path):
         E += (0.5*m*(((path[i]-path[i-1])/dt)**2) + potential(0.5*(path[i]+path[i-1])))
     return E*dt
 
-def metropolis(num,x): #iterates num times to generate optimized paths that start and end at a given x
-    numberOfPaths = num
-    n = 100
+def metropolis(numberOfPaths,x): #iterates num times to generate optimized paths that start and end at a given x
+
     array = np.zeros(shape=(numberOfPaths,N))
+    initialPath = np.random.uniform(-3,3,N) # generate random path
+    initialPath[0] = initialPath[-1] = x # set starting points to x
 
-    for j in range(numberOfPaths):
+    j = 0
+    k = 0
+    while j < numberOfPaths:
 
-        initialPath = np.random.uniform(-3,3,N) # generate random path
-        initialPath[0] = initialPath[-1] = x # set starting points to x
+        perturbedPath = initialPath
+        
+        for i in range(1,len(initialPath)-1): #perturbs our path at each element to create a slightly more optimised path
 
-        for k in range(n):#repeats the thermalization sweep on a path n times
-            perturbedPath = initialPath
-            
-            for i in range(1,len(initialPath)-1): #perturbs our path at each element to create a slightly more optimised path
+            init = perturbedPath[i]
+            a = action(perturbedPath)
+            perturbedPath[i] += np.random.uniform(0,2)-1
+            b = action(perturbedPath)
+            actiondiff = (b-a)
 
-                init = perturbedPath[i]
-                a = action(perturbedPath)
-                perturbedPath[i] += np.random.uniform(0,1)
-                b = action(perturbedPath)
+            if actiondiff < 0: 
+                initialPath = perturbedPath
+            elif np.random.uniform(0,1) < np.exp(-actiondiff):
+                initialPath = perturbedPath
+            else:
+                perturbedPath[i] = init
+        
+        k += 1
+        if k == 100:
+            array[j] = perturbedPath
+            j+=1
+            k = 0
 
-                actiondiff = (b-a)
-
-                if actiondiff < 0: 
-                    initialPath = perturbedPath
-                elif np.random.uniform(0,1) < np.exp(-actiondiff):
-                    initialPath = perturbedPath
-                else:
-                    perturbedPath[i] = init
-                
-        array[j] = perturbedPath
-        #print("Path added",j)
-    #print("Paths Generated")
-    print(array)
     return array
 
 def generateNRandomPaths(numberOfPaths,x):
@@ -93,7 +92,8 @@ def G(paths):#sums action of all paths
 
 
 
-metro = False
+metro = True
+n = 1000
 
 def generateDenominator(): #sum of propagator*dx of path at -3...+3 for example: dx*(G(metropolis(num),-3) + ... + G(metropolis(num),3))
     sum = 0
@@ -101,40 +101,36 @@ def generateDenominator(): #sum of propagator*dx of path at -3...+3 for example:
             if not metro:
                 sum += G(generateNRandomPaths(Iterations,xs2[i]))
             else:
-                sum += G(metropolis(Iterations,xs2[i]))
+                sum += G(metropolis(n,xs2[i]))
     denominator = sum
     return denominator*dx
 
-#denominator = generateDenominator()
-#print(denominator)
-#denominator = 13.424550924328017
-denominator = 136.22891862560073
+denominator = generateDenominator()
+
 def psi(x):
 
     if not metro:
         numerator = G(generateNRandomPaths(Iterations,x)) #brute force paths
     else:
-        numerator = G(metropolis(Iterations,x)) #metropolis paths
+        numerator = G(metropolis(n,x)) #metropolis paths
     probability = numerator/denominator
-    #print("Probability added")
+
     return probability
 
-#print(metropolis(10**4,0))
-#print(G(metropolis(Iterations,xs)))
 def generatePlot():
     array1 = np.zeros(len(xs))
     for i in range(len(xs)):
         array1[i] = psi(xs[i])
     return plt.plot(xs,array1)
 
-moi = generateNRandomPaths(1,0)[0]
-time = np.arange(0,T,dt)/dt
-plt.plot(moi,time)
+#moi = generateNRandomPaths(1,0)[0]
+#time = np.arange(0,T,dt)/dt
+#plt.plot(moi,time)
 
 
-#plt.ylabel("Probability")
-plt.ylabel("Time")
+plt.ylabel("Probability")
+#plt.ylabel("Time")
 plt.xlabel("X-Position")
-#generatePlot()
-#plt.plot(xs,wavefunc(xs))
+generatePlot()
+plt.plot(xs,wavefunc(xs))
 plt.show()
